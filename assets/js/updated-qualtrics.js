a5be2f1c44db51c33865b8ddf58e09aa0db5378b
Alvin G <abgodoy@cpp.edu>
2021-04-06 10:56:53 -0700

    Reworked curricular filter

--- assets/js/updated-qualtrics.js
+++ assets/js/updated-qualtrics.js
@@ -1,24 +1,24 @@
 var app = angular.module('polyXApp', []);
 
 // College division
-app.filter("college_division", function(){
-  return function(input, option){
+app.filter("college_division", function() {
+  return function(input, option) {
     var output = [];
 
     var opt = "";
 
 
-    if (option != null){
+    if (option != null) {
       opt = option;
       if (opt == 'all') {
-        angular.forEach(input, function(value, key){
-            output.push(value);
+        angular.forEach(input, function(value, key) {
+          output.push(value);
         });
-        console.log('option: ' +opt);
+        console.log('option: ' + opt);
         console.log(output);
       } else {
-        angular.forEach(input, function(value, key){
-          if( value.q7 == opt ){
+        angular.forEach(input, function(value, key) {
+          if (value.q7 == opt) {
             output.push(value);
           }
         });
@@ -31,26 +31,38 @@ app.filter("college_division", function(){
 });
 
 // Curricular vs. Co-curricular
-app.filter("curr", function(){
-  return function(input, option){
+app.filter("curr", function() {
+  return function(input, option) {
     var output = [];
 
     var opt = "";
-
-    if(option !== undefined) {
-      opt = option.toLowerCase();
-
-      angular.forEach(input, function(value, key){
-        if(opt === value.q9.toLowerCase()){
-          output.push(value);
+    var tmp = "";
+    var q9 = ""
+    if (option != undefined) {
+      console.log('option: ' + option);
+      angular.forEach(input, function(value, key) {
+        q9 = value.q9.toLowerCase();
+        console.log("q9:  " + q9);
+        console.log('title: ' + value.q1);
+        console.log('type: ' + value.q9);
+        console.log('indexOf q9:  ' + q9.indexOf('curricular'));
+        switch (option) {
+          case 'curricular':
+            if (q9.indexOf('curricular') >= 0 && (q9.length <= 10 || q9.length > 13)) {
+              output.push(value);
+            }
+            break;
+          case 'co-curricular':
+            if (q9.indexOf('co-curricular') >= 0 && (q9.length > 10)) {
+              output.push(value);
+            }
+            break;
+          default:
+            console.log('this is the default');
+            output.push(value);
         }
+      }); //end forEach
 
-        //string length of more than 15 will only collect 'curricular and co-curricular' values
-        if(opt.length > 13 && value.q9.toLowerCase().indexOf(opt) > 0){
-          output.push(value);
-        }
-
-      });
     } else {
       output = input;
     }
@@ -60,22 +72,22 @@ app.filter("curr", function(){
 });
 
 // PolyX name
-app.filter("polyx_name", function(){
-  return function(input, option){
+app.filter("polyx_name", function() {
+  return function(input, option) {
     var output = [];
 
     var opt = "";
 
-    if(option != undefined) {
+    if (option != undefined) {
       opt = option.toLowerCase();
 
-      if (opt == 'all'){
-        angular.forEach(input, function(value, key){
-            output.push(value);
+      if (opt == 'all') {
+        angular.forEach(input, function(value, key) {
+          output.push(value);
         });
       } else {
-        angular.forEach(input, function(value, key){
-          if(opt === value.q1.toLowerCase()){
+        angular.forEach(input, function(value, key) {
+          if (opt === value.q1.toLowerCase()) {
             output.push(value);
           }
 
@@ -90,26 +102,26 @@ app.filter("polyx_name", function(){
 });
 
 // Staff/Faculty in-charge of the PolyX
-app.filter("polyx_contact", function(){
-  return function(input, option){
+app.filter("polyx_contact", function() {
+  return function(input, option) {
     var output = [];
 
     var opt = "";
 
-    if(option != undefined) {
+    if (option != undefined) {
       opt = option.toLowerCase();
 
-      if (opt == 'all'){
-        angular.forEach(input, function(value, key){
-            output.push(value);
+      if (opt == 'all') {
+        angular.forEach(input, function(value, key) {
+          output.push(value);
         });
       } else {
-      angular.forEach(input, function(value, key){
-        if(opt === value.q4.toLowerCase()){
-          output.push(value);
-        }
-      });
-    }
+        angular.forEach(input, function(value, key) {
+          if (opt === value.q4.toLowerCase()) {
+            output.push(value);
+          }
+        });
+      }
     } else {
       output = input;
     }
@@ -119,18 +131,18 @@ app.filter("polyx_contact", function(){
 });
 
 // Class standing
-app.filter("class", function(){
-  return function(input, option){
+app.filter("class", function() {
+  return function(input, option) {
 
     var output = [];
     var opt = "";
 
-    if(option != undefined) {
+    if (option != undefined) {
       opt = option.toLowerCase();
 
-      angular.forEach(input, function(value, key){
+      angular.forEach(input, function(value, key) {
 
-        if( value.q12.toLowerCase().indexOf(opt) > 0 ){
+        if (value.q12.toLowerCase().indexOf(opt) > 0) {
           output.push(value);
         }
 
@@ -147,7 +159,7 @@ app.filter("class", function(){
 app.controller('PolyXController', function PolyXController($scope, $http) {
 
   var jsonFeed = 'assets/docs/updated-qualtrics.json';
-  $http.get(jsonFeed).then(function(response){
+  $http.get(jsonFeed).then(function(response) {
     $scope.polyXList = response.data;
     console.log(response.data);
 
@@ -161,19 +173,22 @@ app.controller('PolyXController', function PolyXController($scope, $http) {
 //**********SUBROUTINES TO AUTOMATE FORM INPUTS**********//
 
 // get unique contacts list for dropdown menu
-function getUniqueContacts(arr){
-  var contact={};
+function getUniqueContacts(arr) {
+  var contact = {};
   var contacts = [];
   var uniques = [];
 
-  for(i=0; i<arr.entries.length; i++){
-    var fullName=arr.entries[i].q5 +", " +arr.entries[i].q6;
-    var email=arr.entries[i].q4;
+  for (i = 0; i < arr.entries.length; i++) {
+    var fullName = arr.entries[i].q5 + ", " + arr.entries[i].q6;
+    var email = arr.entries[i].q4;
 
     // check if email exists on the list
-    if(uniques.indexOf(email) == -1){
+    if (uniques.indexOf(email) == -1) {
       uniques.push(email);
-      contact = {fullName:fullName, email:email};
+      contact = {
+        fullName: fullName,
+        email: email
+      };
       contacts.push(contact);
     }
   }
@@ -184,10 +199,10 @@ function getUniqueContacts(arr){
 }
 
 // get Polytechnic Experience Titles for dropdown menu
-function getPolyXTitles(arr){
-  var titles=[];
+function getPolyXTitles(arr) {
+  var titles = [];
 
-  for(i=0; i<arr.entries.length; i++){
+  for (i = 0; i < arr.entries.length; i++) {
     titles.push(arr.entries[i].q1);
   }
 
@@ -196,14 +211,14 @@ function getPolyXTitles(arr){
 }
 
 // get Unique Division for the dropdown menu
-function getUniqueDivisions(arr){
-  var divisions=[];
-  var uniques=[];
+function getUniqueDivisions(arr) {
+  var divisions = [];
+  var uniques = [];
 
-  for(i=0; i<arr.entries.length; i++){
-    var division=arr.entries[i].q7;
+  for (i = 0; i < arr.entries.length; i++) {
+    var division = arr.entries[i].q7;
 
-    if(uniques.indexOf(division) == -1){
+    if (uniques.indexOf(division) == -1) {
       uniques.push(division);
       divisions.push(division);
     }
